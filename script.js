fe0071a51d053e51c3d8e212f0b2879443c6b1dd
Eddy518 <edmwangi2@gmail.com>
2023-10-17 11:06:52 +0300

    feat: Add calculator functions to operate on numbers

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const calcDisplay = document.querySelector(".calc-display");
-const calcBtns = document.querySelectorAll(".calc-row");
+const calcBtns = document.querySelectorAll(".btn");
 calcDisplay.textContent = 0;
 
 calcBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    calcDisplay.textContent = this.textContent;
+    calcDisplay.textContent += btn.textContent;
   });
 });
+function add(num1, num2) {
+  return num1 + num2;
+}
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+    default:
+      console.log("Invalid operator or operand");
+  }
+}
+const result = operate(3, "+", 2);
+console.log(result);
+/*  DEBUGGING */
+const reload = document.querySelector("#reload");
+reload.addEventListener("click", () => location.reload());
