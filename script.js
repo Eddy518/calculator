d78c17017a210240811eeded70f78e19c5f2d38b
Eddy518 <edmwangi2@gmail.com>
2023-12-23 13:35:06 +0300

    feat: Round off excess numbers to 10dp

--- script.js
+++ script.js
@@ -45,21 +45,32 @@ function operate(num1, operator, num2) {
       console.log("Invalid operator or operand");
   }
 }
+
+function clearAllCalc() {
+  result = null; // this prevents from using previous result for next calculation in line 85
+  flag = false;
+  operatorFlag = false;
+  num1Status = false;
+  num1 = [];
+  num2 = [];
+}
+function clearCalc() {}
 function playSound() {
   buttonSound.currentTime = 0; // * Allow for playing multiple times
   buttonSound.play();
 }
+
 function calculate(num1, num2, operator) {
   let first = Number(num1.join(""));
   let second = Number(num2.join(""));
-  console.log(`num1 is ${first} with type ${typeof first}`);
-  console.log(`num2 is ${second} with type ${typeof second}`);
   result = operate(first, operator, second);
+  if (!Number.isInteger(result)) {
+    result = result.toFixed(10);
+  }
   calcDisplay.textContent = result;
-  console.log(num1);
-  //  num1.splice(0, num1.length, result); //lol
+  //console.log(num1);
   operator = null;
-  console.log(result);
+  //console.log(result);
 }
 
 calcNumbers.forEach((btn) => {
@@ -90,48 +101,40 @@ calcOperators.forEach((btn) => {
     //      calculate(num1, num2, operator);
     //    }
     */
-    console.log(operator);
-    console.log(`Result is ${result}`);
+    //console.log(operator);
+    //console.log(`Result is ${result}`);
     if (result) {
       //if a calculation is made after previous calculation
-      console.log(`If result is true num1 is ${num1}`);
+      // console.log(`If result is true num1 is ${num1}`);
       num1.splice(0, num1.length);
       num2.splice(0, num2.length);
       num1.push(result);
-      console.log(`test ${num1}`);
+      //console.log(`test ${num1}`);
     }
-    console.log(`num1 after operator ${num1.join("")}`);
+    //console.log(`num1 after operator ${num1.join("")}`);
     num1Status = true;
-    console.log(`num1 is ${num1}`);
-    console.log(`num2 is ${num2}`);
+    //console.log(`num1 is ${num1}`);
+    //console.log(`num2 is ${num2}`);
     if (flag === false && operator === "-") {
       calcDisplay.textContent = "";
       flag = true;
     }
     calcDisplay.textContent += btn.dataset.operator;
-    console.log(num1);
+    //console.log(num1);
   });
 });
 
 calcEquals.addEventListener("click", () => {
   calculate(num1, num2, operator);
 });
+
 calcAllClear.addEventListener("click", () => {
   calcDisplay.textContent = "";
   calcDisplay.textContent = 0;
-  result = null; // this prevents from using previous result for next calculation in line 85
-  flag = false;
-  operatorFlag = false;
-  num1Status = false;
-  num1 = [];
-  num2 = [];
+  clearAllCalc();
 });
+
 calcClear.addEventListener("click", () => {
-  //  if (calcDisplay.textContent.length > 1) {
-  //    calcDisplay.textContent = calcDisplay.textContent.slice(0, -1); //*Remove the last string from the display
-  //  } else {
-  //    calcDisplay.textContent = 0;
-  //  }
   //check if array is num1 or num2 and pop it
   num1Status === false ? num1.pop() : num2.pop();
   flag = false;
@@ -139,8 +142,9 @@ calcClear.addEventListener("click", () => {
     calcDisplay.textContent.length > 1
       ? calcDisplay.textContent.slice(0, -1) // * Remove the last string from the display
       : 0;
-  console.log(num1, num2);
+  //console.log(num1, num2);
 });
+
 calcDecimal.addEventListener("click", (e) => {
   calcDisplay.textContent += e.target.dataset.decimal;
 });
