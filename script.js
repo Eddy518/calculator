db700bd974c3649ce05b2a0694f9e7c84febb345
Eddy518 <edmwangi2@gmail.com>
2023-12-28 15:44:22 +0300

    feat: Add keyboard support to numbers and operator

--- script.js
+++ script.js
@@ -111,6 +111,44 @@ function clearDigit() {
   }
 }
 
+function getExponent() {
+  let num;
+  if (!num1Status) {
+    num = Number(num1.join(""));
+    const exponentNum = "" + num ** 2; //convert num to string
+    num1 = [];
+    num1.push(exponentNum);
+    calcDisplay.textContent = exponentNum;
+  } else {
+    const length = num2.length;
+    num = Number(num2.join(""));
+    const exponentNum = "" + num ** 2; //convert num to string
+    num2 = [];
+    num2.push(exponentNum);
+    calcDisplay.textContent =
+      calcDisplay.textContent.slice(0, -length) + exponentNum;
+  }
+}
+
+function getPercentage() {
+  let num;
+  if (!num1Status) {
+    num = Number(num1.join(""));
+    const percentNum = "" + num / 100; //convert num to string
+    num1 = [];
+    num1.push(percentNum);
+    calcDisplay.textContent = percentNum;
+  } else {
+    const length = num2.length;
+    num = Number(num2.join(""));
+    const percentNum = "" + num / 100; //convert num to string
+    num2 = [];
+    num2.push(percentNum);
+    calcDisplay.textContent =
+      calcDisplay.textContent.slice(0, -length) + percentNum;
+  }
+}
+
 calcNumbers.forEach((btn) => {
   btn.addEventListener("click", () => {
     playSound();
@@ -158,41 +196,11 @@ calcAllClear.addEventListener("click", () => {
 });
 
 calcPercent.addEventListener("click", () => {
-  let num;
-  if (!num1Status) {
-    num = Number(num1.join(""));
-    const percentNum = "" + num / 100; //convert num to string
-    num1 = [];
-    num1.push(percentNum);
-    calcDisplay.textContent = percentNum;
-  } else {
-    const length = num2.length;
-    num = Number(num2.join(""));
-    const percentNum = "" + num / 100; //convert num to string
-    num2 = [];
-    num2.push(percentNum);
-    calcDisplay.textContent =
-      calcDisplay.textContent.slice(0, -length) + percentNum;
-  }
+  getPercentage();
 });
 
 calcExponent.addEventListener("click", () => {
-  let num;
-  if (!num1Status) {
-    num = Number(num1.join(""));
-    const exponentNum = "" + num ** 2; //convert num to string
-    num1 = [];
-    num1.push(exponentNum);
-    calcDisplay.textContent = exponentNum;
-  } else {
-    const length = num2.length;
-    num = Number(num2.join(""));
-    const exponentNum = "" + num ** 2; //convert num to string
-    num2 = [];
-    num2.push(exponentNum);
-    calcDisplay.textContent =
-      calcDisplay.textContent.slice(0, -length) + exponentNum;
-  }
+  getExponent();
 });
 
 calcClear.addEventListener("click", () => {
@@ -217,13 +225,62 @@ calcDecimal.addEventListener("click", (e) => {
 
 function checkKey(e) {
   console.log(e);
-  switch (e.key) {
-    case "Backspace":
-      clearDigit();
-      break;
-    case "c":
-      clearAllCalc();
-      break;
+  const num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const operatorArr = ["*", "+", "-", "/"];
+
+  if (num.includes(e.key)) {
+    playSound();
+
+    if (flag === false) {
+      calcDisplay.textContent = "";
+      flag = true;
+    }
+    calcDisplay.textContent += e.key;
+    if (num1Status === false) {
+      // If operator is clicked move next set of numbers to second array??
+      num1.push(e.key);
+    } else {
+      // operator = null;
+      num2.push(e.key);
+    }
+  }
+
+  if (operatorArr.includes(e.key)) {
+    console.log("includes operator");
+    playSound();
+    if (!operator) {
+      //only if there is no operator
+      operator = e.key;
+      num1Status = true; //if operator is clicked then that means it's time to move to the next array
+      if (flag === false && operator === "-") {
+        calcDisplay.textContent = "";
+        flag = true;
+      }
+    } else {
+      calculate(num1, num2, operator);
+      operator = e.key;
+    }
+    calcDisplay.textContent += e.key;
+  }
+
+  if (e.key === "Backspace") {
+    clearDigit();
+  }
+
+  if (e.key === "=" || e.key === "Enter") {
+    calculate(num1, num2, operator);
+  }
+
+  if (e.key === "c") {
+    clearAllCalc();
+  }
+
+  if (e.key === "p") {
+    getExponent();
+  }
+
+  if (e.key === "%") {
+    getPercentage();
   }
 }
 document.addEventListener("keydown", checkKey);
